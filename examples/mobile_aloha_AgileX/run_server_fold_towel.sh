7c4a42a735af9c4e65876735b5d81a7342039f3e
xianglunkai <1322099375@qq.com>
2025-11-10 17:59:00 +0800

    develope for mobile-navigation

--- examples/mobile_aloha_AgileX/run_server_fold_towel.sh
+++ examples/mobile_aloha_AgileX/run_server_fold_towel.sh
@@ -2,5 +2,5 @@
 export HF_ENDPOINT=https://hf-mirror.com
 uv run scripts/serve_policy.py \
     --env ALOHA \
-    --default_prompt='Carefully fold the towel and then place the folded towel on the black notebook' \
-    policy:checkpoint --policy.config=pi05_cobot --policy.dir=/home/xlk/work/openpi/checkpoints/pi05_cobot/pi05_cobot_fold_towel/checkpoint-90k \
+    --default_prompt='lay the towel flat, then carefully fold the towel and then place the folded towel on the black notebook' \
+    policy:checkpoint --policy.config=pi05_cobot_fold_towel --policy.dir=/home/xlk/work/openpi/checkpoints/pi05_cobot/pi05_cobot_fold_towel/checkpoint-90k 
---
+++ jax2torch.sh
@@ -0,0 +1,4 @@
+uv run examples/convert_jax_model_to_pytorch.py \
+    --checkpoint_dir /home/xlk/work/openpi/checkpoints/pi05_cobot/pi05_cobot_fold_towel/checkpoint-90k \
+    --config_name pi05_cobot_fold_towel \
+    --output_path /home/xlk/work/openpi/checkpoints/pi05_cobot/pi05_cobot_fold_towel/checkpoint-90k-converted
