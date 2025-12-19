2d0217baf4120c3c2543e3c84ca18ec19ae8be5f
xianglunkai <1322099375@qq.com>
2025-12-19 14:07:28 +0800

    update code

---
+++ examples/mobile_aloha_AgileX/run_server_fold_towel_torch.sh
@@ -0,0 +1,6 @@
+# uv run scripts/serve_policy.py --env ALOHA --default_prompt='Pick up the bottle on the table headup with the correct arm'
+export HF_ENDPOINT=https://hf-mirror.com
+uv run scripts/serve_policy.py \
+    --env COBOT \
+    --default_prompt='lay the towel flat, then carefully fold the towel and then place the folded towel on the black notebook' \
+    policy:checkpoint --policy.config=pi05_cobot_fold_towel --policy.dir=/home/xlk/work/openpi/checkpoints/pi05_cobot/pi05_cobot_fold_towel/checkpoint-90k-converted 
--- jax2torch.sh
+++ jax2torch.sh
@@ -1,5 +1,11 @@
+# uv run examples/convert_jax_model_to_pytorch.py \
+#     --checkpoint_dir /home/xlk/work/openpi/checkpoints/pi05_cobot/pi05_cobot_fold_shirt/checkpoint-15k \
+#     --config_name pi05_cobot_fold_shirt \
+#     --output_path /home/xlk/work/openpi/checkpoints/pi05_cobot/pi05_cobot_fold_shirt/checkpoint-15k-converted \
+#     --precision bfloat16
+
 uv run examples/convert_jax_model_to_pytorch.py \
-    --checkpoint_dir /home/xlk/work/openpi/checkpoints/pi05_cobot/pi05_cobot_fold_shirt/checkpoint-15k \
-    --config_name pi05_cobot_fold_shirt \
-    --output_path /home/xlk/work/openpi/checkpoints/pi05_cobot/pi05_cobot_fold_shirt/checkpoint-15k-converted \
+    --checkpoint_dir /home/xlk/work/openpi/checkpoints/pi05_cobot/pi05_cobot_fold_towel/checkpoint-90k \
+    --config_name pi05_cobot_fold_towel \
+    --output_path /home/xlk/work/openpi/checkpoints/pi05_cobot/pi05_cobot_fold_towel/checkpoint-90k-converted \
     --precision bfloat16
