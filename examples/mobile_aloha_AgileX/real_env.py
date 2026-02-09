73359e0cbc56b3315d86345be9612ab44f5b3a20
xianglunkai <1322099375@qq.com>
2026-02-09 14:07:51 +0800

    fix griper default value

--- examples/mobile_aloha_AgileX/real_env.py
+++ examples/mobile_aloha_AgileX/real_env.py
@@ -229,8 +229,8 @@ class RealEnv:
             vel_action_target = np.array(vel_action, dtype=float)
         
         # ! useful for pour water
-        left_arm_target[6] =  left_arm_target[6].copy() -0.035
-        right_arm_target[6] = right_arm_target[6].copy() - 0.035
+        left_arm_target[6] =  left_arm_target[6].copy() -0.0035
+        right_arm_target[6] = right_arm_target[6].copy() - 0.0035
       
         #! useful for adjust bottle task
         # left_arm_target[6] = tanh_smooth_map(left_arm_target[6].copy())   # Left arm gripper
